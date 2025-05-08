f067d317d7310505217dd5449d877802d2ecfcaa
Hieu Ho <hvhieu.dev@gmail.com>
2025-05-08 11:22:26 +0700

    How to understand the setting state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,9 +6,21 @@ class MyComponent extends React.Component {
         address: 'Saigon',
         age: 33
     } // State is only a javascript object
+
+    handleClick(event) {
+        // merge State => only React class
+        this.setState({
+            name: 'Cris',
+            age: 50
+        })
+        // console.log(event.target)
+    }
+
     render() {
         return (
-            <div>My first name is {this.state.name} and I'm from {this.state.address}</div>
+            <div>My first name is {this.state.name} and I'm from {this.state.address}
+                <button onClick={(event) => { this.handleClick(event) }}>Click Me</button>
+            </div>
         );
     }
 }
