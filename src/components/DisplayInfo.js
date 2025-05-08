750c20ed03e1622cacf050f1c35e67fa9e0a2947
Hieu Ho <hvhieu.dev@gmail.com>
2025-05-08 14:59:07 +0700

    Conditional output

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,26 +1,43 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+    state = {
+        isShowListUser: true
+    }
+    handleShowHideListUser = (event) => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
     render() {
         //pros => properties
         //destruturing array/object
         // const { name, age } = this.props;
         const { listUsers } = this.props;
-
+        // console.table(listUsers)
         return (
             <div>
-                {listUsers.map((user) => {
-                    return (
-                        <div key={user.id}>
-                            <div>My name's {user.name}</div>
-                            <div>My age's {user.age}</div>
-                            <hr />
-                        </div>
-                    )
-                })}
+                <div>
+                    <span onClick={(event) => { this.handleShowHideListUser(event) }}>{this.state.isShowListUser ? 'Hide' : 'Show'} list user</span>
+                </div>
+                {this.state.isShowListUser &&
+                    < div >
+                        {
+                            listUsers.map((user) => {
+                                return (
+                                    <div key={user.id} className={+user.age > 34 ? 'red' : 'green'}>
+                                        <div>My name's {user.name}</div>
+                                        <div>My age's {user.age}</div>
+                                        <hr />
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                }
                 {/* <div>My name's {name}</div>
                 <div>My age's {age}</div> */}
-            </div>
+            </div >
         )
     }
 }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,7 +5,7 @@ import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
     state = {
         listUsers: [
-            { id: 1, name: 'Hieu', age: 30 },
+            { id: 1, name: 'Hieu', age: 15 },
             { id: 2, name: 'Cris', age: 33 },
             { id: 3, name: 'Dev', age: 35 }
         ]
