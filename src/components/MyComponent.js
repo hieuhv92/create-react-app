631f1793a33e29db97fafdb22f2a9078c2bc8851
Hieu Ho <hvhieu.dev@gmail.com>
2025-05-08 10:27:32 +0700

    How to use the react state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = { // the way that React controls data of Componment
+        name: 'Hieu Ho',
+        address: 'Saigon',
+        age: 33
+    } // State is only a javascript object
     render() {
         return (
-            <div>My first component!!!!</div>
+            <div>My first name is {this.state.name} and I'm from {this.state.address}</div>
         );
     }
 }
