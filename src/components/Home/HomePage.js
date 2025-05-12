e1fafed76e7d0b36f677617e391a73ae9459585e
Hieu Ho <hvhieu.dev@gmail.com>
2025-05-12 16:43:06 +0700

    Adding a video to homepgae

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,6 +1,13 @@
+import VideoHomePage from '../../assets/video-homepage.mp4'
+
 const HomePage = () => {
     return (
-        <div className="homepage-container">HomePage Component</div>
+        <div className="homepage-container">
+            <div>HomePage Component</div>
+            <video loop autoPlay muted>
+                <source src={VideoHomePage} type="video/mp4" />
+            </video>
+        </div>
     )
 
 }
