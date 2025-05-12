3ee61e88934ae9fde446018d9744997a99408cec
Hieu Ho <hvhieu.dev@gmail.com>
2025-05-12 15:11:48 +0700

    Adding react router and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
-import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='app-container'>
-        <MyComponent></MyComponent>
-      </div>
-    )
-  }
+// Use class component => old
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className='app-container'>
+//         <MyComponent></MyComponent>
+//       </div>
+//     )
+//   }
+// }
+
+//Using function component => new, use react hook to replace using state
+const App = () => {
+  return (
+    <div className="app-container">
+      Hello World!!
+    </div>
+  );
 }
-
-// const App = () => {
-//   // const count = useSelector(state => state.counter.count);
-//   // const dispatch = useDispatch();
-
-//   return (
-//     <div>
-//       <MyComponent></MyComponent>
-//     </div>
-//   );
-// }
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
