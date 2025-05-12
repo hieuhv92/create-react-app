cea8a2e76757c39304c011248675c5dd5cbebaef
Hieu Ho <hvhieu.dev@gmail.com>
2025-05-12 17:08:06 +0700

    Styling CSS for Header

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -4,6 +4,13 @@ const HomePage = () => {
     return (
         <div className="homepage-container">
             <div>HomePage Component</div>
+            <div className='homepage-content'>
+                <div className='title-1'> There's a better way to ask</div>
+                <div className='title-2'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+                <div className='title-3'>
+                    <button>Get's started.It's free</button>
+                </div>
+            </div>
             <video loop autoPlay muted>
                 <source src={VideoHomePage} type="video/mp4" />
             </video>
