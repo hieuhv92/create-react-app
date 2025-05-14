107b5f5a53f8cde01cf11ada4d8d367bca7c7540
Hieu Ho <hvhieu.dev@gmail.com>
2025-05-14 22:10:44 +0700

    Handling login functionality

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -2,7 +2,6 @@ import { FaBars } from "react-icons/fa";
 import { useState } from "react";
 import SideBar from "./SideBar";
 import { Outlet } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 
 const Admin = (props) => {
     const [collapse, setCollapse] = useState(false);
@@ -19,17 +18,6 @@ const Admin = (props) => {
                     <Outlet />
                 </div>
             </div>
-            <ToastContainer
-                position="top-right"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick={false}
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
         </div>
     )
 }
---
+++ src/components/Auth/Login.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import './Login.scss';
+import { useNavigate } from 'react-router-dom';
+import { postLogin } from '../../services/ApiServices';
+import { toast } from 'react-toastify';
+
+const Login = () => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        const data = await postLogin(email, password);
+
+        if (data && +data.EC === 0) {
+            toast.success(data.EM);
+            navigate('/');
+        }
+        if (data && +data.EC !== 0) {
+            toast.error(data.EM);
+        }
+    }
+
+    return (
+        <div className="login-container">
+            <div className='header'>
+                <span>Don't you have an account yet?</span>
+                <button>Sign up</button>
+            </div>
+            <div className='title'>
+                Hieu Ho IT
+            </div>
+            <div className='welcome'>
+                Hello, who's this?
+            </div>
+            <div className='content-form col-4 mx-auto'>
+                <div className='form-group'>
+                    <label>Email</label>
+                    <input
+                        type={'email'}
+                        className='form-control'
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                </div>
+                <div className='form-group'>
+                    <label>Password</label>
+                    <input
+                        type={'password'}
+                        className='form-control'
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div>
+                <span className='forgot-password'>Forgot Password?</span>
+                <div>
+                    <button onClick={() => handleLogin()} className='btn-submit'>Log in</button>
+                </div>
+                <div className='text-center'>
+                    <span className='back-home' onClick={() => navigate('/')}> &#60;&#60; Go to Homepage</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
