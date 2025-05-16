049f32679ece09624cea1d6828a1046393bea101
Hieu Ho <hvhieu.dev@gmail.com>
2025-05-16 17:02:10 +0700

    Processing data on client side

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getQuizData } from '../../services/ApiServices'
+import _ from 'lodash';
 
 const DetailQuiz = () => {
     const params = useParams();
@@ -12,7 +13,27 @@ const DetailQuiz = () => {
 
     const fetchQuestionData = async () => {
         const reponse = await getQuizData(quizId);
-        console.log(reponse);
+        if (reponse && reponse.EC === 0) {
+            var data = _.chain(reponse.DT)
+                // Group the elements of Array based on `color` property
+                .groupBy("id")
+                // `key` is group's name (color), `value` is the array of objects
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescrition, image = null;
+
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescrition = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers);
+                    })
+
+                    return { questionId: key, answers, questionDescrition, image }
+                })
+                .value()
+        }
     }
 
     return (
