d24d87121b70a317f0b9eca4acc3cb81089525b3
Hieu Ho <hvhieu.dev@gmail.com>
2025-05-17 17:14:40 +0700

    Handling data before submitting API

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -75,6 +75,30 @@ const DetailQuiz = (props) => {
         }
     }
 
+    const handleFinishQuiz = () => {
+        let payLoad = {
+            quizId: +quizId,
+            "answers": []
+        }
+        let answers = [];
+        if (dataQuiz && dataQuiz.length > 0) {
+            dataQuiz.forEach(question => {
+                let userAnswerId = [];
+                question.answers.forEach(a => {
+                    if (a.isSelected) {
+                        userAnswerId.push(a.id);
+                    }
+                })
+                answers.push({
+                    questionId: +question.questionId,
+                    userAnswerId: userAnswerId
+                })
+            })
+            payLoad.answers = answers;
+            console.log(payLoad);
+        }
+    }
+
     return (
         <div className="detail-quiz-container">
             <div className="left-content">
@@ -95,6 +119,7 @@ const DetailQuiz = (props) => {
                 <div className="q-footer">
                     <button className="btn btn-secondary" onClick={() => handlePrev()}>Prev</button>
                     <button className="btn btn-primary" onClick={() => handleNext()}>Next</button>
+                    <button className="btn btn-primary" onClick={() => handleFinishQuiz()}>Finish</button>
                 </div>
             </div>
             <div className="right-content">
